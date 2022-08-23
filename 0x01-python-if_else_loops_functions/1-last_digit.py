6257d98035c6e59586a8beaad60a73145e25973e
MercyChebet <chebetm96@gmail.com>
2022-08-23 10:32:20 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,45 @@
-
-
 #!/usr/bin/python3
+
 import random
+
 number = random.randint(-10000, 10000)
-if(number < 0):
-        lastDigit = int(str(number)[-1]) * -1
-    else:
-            lastDigit = int(str(number)[-1])
-            first = "Last digit of "
-            last = " and is less than 6 and not 0"
-            if(lastDigit > 5):
-                    print(first + "{} is {} and is greater than 5".format(number, lastDigit))
-                elif(lastDigit == 0):
-                        print(first + "{} is {} and is 0".format(number, lastDigit))
-                    else:
-                            print(first + "{} is {}".format(number, lastDigit) + last)
+
+no = number
+
+if no < 0:
+
+    no *= -1
+
+last_digit = no % 10
+
+if last_digit > 5 and number < 0:
+
+    last_digit *= -1
+
+    print(f"Last digit of {number:d} is {last_digit:d}"
+
+          f" and is less than 6 and not 0")
+
+elif last_digit > 5 and number > 0:
+
+    print(f"Last digit of {number:d} is {last_digit:d}"
+
+          f" and is greater than 5")
+
+elif last_digit == 0:
+
+    print(f"Last digit of {number:d} is {last_digit:d} and is 0")
+
+elif last_digit < 6 and last_digit != 0 and number < 0:
+
+    last_digit *= -1
+
+    print(f"Last digit of {number:d} is {last_digit:d}"
+
+          f" and is less than 6 and not 0")
+
+elif last_digit < 6 and last_digit != 0 and number > 0:
+
+    print(f"Last digit of {number:d} is {last_digit:d}"
+
+          f" and is less than 6 and not 0")
