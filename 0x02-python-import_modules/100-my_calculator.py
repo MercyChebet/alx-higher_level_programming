4719bce0df137d5c74a71349e81b409b4c1243db
MercyChebet <chebetm96@gmail.com>
2022-08-26 00:36:23 -0700

    a program that imports all functions from the file calculator_1.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,42 +1,20 @@
-
-m sys import argv
-
-from calculator_1 import add, sub, mul, div
-
-if __name__ == "__main__":
-
-    argc = len(argv) - 1
-
-    if argc != 3:
-
+f __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-
         exit(1)
-
-    operator = argv[2]
-
-    a = int(argv[1])
-
-    b = int(argv[3])
-
-    if operator == "+":
-
-        print("{} + {} = {}".format(a, b, add(a, b)))
-
-    elif operator == "-":
-
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-
-    elif operator == "*":
-
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-
-    elif operator == "/":
-
-        print("{} / {} = {}".format(a, b, div(a, b)))
-
+    if argv[2] in {"+", "-", "/", "*"}:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        if argv[2] == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        if argv[2] == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        if argv[2] == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-
         print("Unknown operator. Available operators: +, -, * and /")
-
         exit(1)
