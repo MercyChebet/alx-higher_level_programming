5ec350035330ddc0eeed7ef83926618ab244c9b4
MercyChebet <chebetm96@gmail.com>
2022-09-26 10:46:23 -0700

    a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-"""Module 11-square.
-Creates a Square class.
+"""
+    11-square: class Square from Rectangle
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square.
-    Private instance attribute size.
-    Public method area().
-    Inherits from Rectangle.
     """
-
+        Square  inherits from Rectangle
+        Attributes:
+            size (int): side of square
+        Methods:
+            __init__ - initialises the square
+    """
     def __init__(self, size):
-        """Initializes a Square.
-        Args:
-            - size: size of the square
         """
-
+            initialises Square
+        """
         self.integer_validator("size", size)
-        super().__init__(size, size)
+
         self.__size = size
 
-    def __str__(self):
-        return str("[Square] {}/{}".format(self.__size, self.__size))
-
     def area(self):
-        """Computes the area of a Square instance.
-        Overwrites the area() method from Rectangle.
         """
+            Returns the area of square
+        """
+        area = self.__size * self.__size
+        return area
 
-        return self.__size ** 2
+    def __str__(self):
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                    self.__size, self.__size))
