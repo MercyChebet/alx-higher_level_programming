8c81d4323107ae73ec7e3d061581c62988bc4374
MercyChebet <chebetm96@gmail.com>
2022-09-26 10:26:49 -0700

    a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).

--- 0x0A-python-inheritance/tests/8-rectangle.py
+++ 0x0A-python-inheritance/tests/8-rectangle.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Module 7-base_geometry.
-Creates a BaseGeometry class.
-"""
+"""inherits from BaseGeometry"""
 
 
 class BaseGeometry:
-    """Class with public instance methods."""
+    """public instance method"""
 
     def area(self):
-        """Raises an Exception with the message
-        'area() is not implemented'.
-        """
-
-        raise Exception('area() is not implemented')
+        """raises an exception"""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value."""
-
+        """validates value"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initialization of privates"""
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
