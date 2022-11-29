21e475d9b034d2bfc08dd29cd6bbee206e1d8a12
MercyChebet <chebetm96@gmail.com>
2022-11-29 11:44:43 -0800

    Advanced

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+const initDict = require('./101-data').dict;
+const newDict = {};
+
+for (const key in initDict) {
+  if (newDict[initDict[key]] === undefined) {
+    newDict[initDict[key]] = [];
+  }
+  newDict[initDict[key]].push(key);
+}
+console.log(newDict);
