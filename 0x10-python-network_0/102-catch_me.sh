a34f06879e195e6b8d1e4869407bb12f365bf46d
MercyChebet <chebetm96@gmail.com>
2022-12-15 10:32:09 -0800

    Advanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-# Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:You got me!"
+cript that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sX PUT -d -L 'user_id=98' -H "Origin: You got me!" 0.0.0.0:5000/catch_me
